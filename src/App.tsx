db1ec58c8bf7173ea5fb8a3a28db2224fa93d085
Alayande Esther <alayandeesther469@gmail.com>
2025-12-10 17:26:48 +0100

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Hero from '@/components/landing-page/Hero'
 import Faq from './components/landing-page/Faq'
 import GetInvolved from '@/components/landing-page/GetInvolved'
-import Intro from '@/components/landing-page/Intro'
 import NewsSection from './components/landing-page/News'
 import PartnersSection from './components/landing-page/Partners'
 import OurPrograms from './components/landing-page/OurPrograms'
@@ -15,7 +14,6 @@ const App:React.FC = () => {
     <div>
   
       <Hero/>
-      <Intro/>
       <HeroSection/>
       <OurPrograms/>
       <PartnersSection/>
